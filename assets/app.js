eb7f3ff3e27e49adbe1d8c1f7228f5ffbd5e71f1
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-18 21:49:07 +0800

    linked files + jquerry + materialize + bkg img

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,28 @@
-// 
+//this is to load the content
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+
+
+//collect search data from input
+
+//create a variable
+
+//fetch characters with the use of the user input variable
+
+//display name as title of container/ creat element and append to container
+
+//display main picture left hand bio and append to container
+
+//display bio as p element appended to container
+
+//dislay alter-ego picture right hand top conrner as img
+
+//fetch IMBD request for movies based on search data
+
+//(we may need to hard code and check if the same searches bring up relevant data)
+
+//display this underneath bio
+
