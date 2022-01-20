dd64f2ca17a1ccf35fff699d7f5e16c7ff9c6a6e
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-20 19:30:54 +0800

    temp background + marvel red added to search

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,10 @@
-//this is to load the content
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
+//this is to load the content unsure if needed 
+//var instance = M.Parallax.getInstance(elem);
+//
+//document.addEventListener('DOMContentLoaded', function() {
+//    var elems = document.querySelectorAll('.parallax');
+//    var instances = M.Parallax.init(elems, options);
+//  });
 
 
 
