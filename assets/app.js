25071b6a54e973b0eff96543e961e3656bf2930a
hannah AB <hannah.allisonbatt@gmail.com>
2022-01-24 13:07:04 +0800

    new draft + click event for all characters

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,29 @@
-//this is to load the content unsure if needed 
-//var instance = M.Parallax.getInstance(elem);
-//
-//document.addEventListener('DOMContentLoaded', function() {
-//    var elems = document.querySelectorAll('.parallax');
-//    var instances = M.Parallax.init(elems, options);
-//  });
+const landingbtn = document.getElementById("center-btn");
+const nav = document.getElementById("header");
+const cardContainer = document.getElementById("charcter-card-container");
+
+cardContainer.classList.add("hide");
+
+landingbtn.addEventListener("click", function(event){
+    landingbtn.classList.add("hide");
+    nav.classList.add("hide");
+    cardContainer.classList.remove("hide");
+    const choice = event.target;
+    const groupChoice = choice.getAttribute("data-character");
+    
+    if (groupChoice == "avengers"){
+        //run functions for avenger
+    }
+    if (groupChoice == "villans"){
+        //run function for villans
+    }
+    if (groupChoice == "guardians"){
+        //run function for guardians
+    }
+    if (groupChoice == "x-men"){
+        //run function for x-men
+    }
 
 
-
-//collect search data from input
-
-//create a variable
-
-//fetch characters with the use of the user input variable
-
-//display name as title of container/ creat element and append to container
-
-//display main picture left hand bio and append to container
-
-//display bio as p element appended to container
-
-//dislay alter-ego picture right hand top conrner as img
-
-//fetch IMBD request for movies based on search data
-
-//(we may need to hard code and check if the same searches bring up relevant data)
-
-//display this underneath bio
+});
 
