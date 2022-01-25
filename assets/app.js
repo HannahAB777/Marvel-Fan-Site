7f0bcfaacdcd4762fdf6129ac06c601a6b0c275a
MichaelFellas <MichaelAriti94@gmail.com>
2022-01-25 14:47:33 +0800

    Update

--- assets/app.js
+++ assets/app.js
@@ -319,4 +319,58 @@ function searchmoviesArray (array){
 
 
 
+function createCard(icon, name, description){
+  const card = document.createElement("div");
+  card.classList.add("row");
+  const cardBorder = document.createElement("div");
+  cardBorder.classList.add("col s12 m7");
+  card.appendChild(cardBorder);
+  const cardEl = document.createElement("div");
+  cardEl.classList.add("card");
+  cardBorder.appendChild(cardEl);
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("card-image");
+  cardEl.appendChild(imageDiv);
+  const heroImage = document.createElement("img");
+  heroImage.setAttribute("src", icon);
+  imageDiv.appendChild(img);
+  const heroName = document.createElement("span");
+  heroName.classList.add("card-title");
+  heroName.textContent = name;
+  cardEl.appendChild(heroName);
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card-content");
+  cardEl.appendChild(cardContent);
+  const heroDescription = document.createElement("p");
+  heroDescription.textContent = description;
 
+}
+
+function movieCard(cardContent, movie, year, plot, IMBD, rating, link) {
+
+  const movieInfo = document.createElement("ul");
+  cardContent.appendChild(movieInfo);
+  const movieTitle = document.createElement("li");
+  movieTitle.textContent = movie;
+  movieInfo.appendChild(movieTitle);
+  const movieYear = document.createElement("li");
+  movieYear.textContent = year;
+  movieInfo.appendChild(movieYear);
+  const plotShort = document.createElement("li");
+  plotShort.textContent = plot;
+  movieInfo.appendChild(plotShort);
+  const IMBDId = document.createElement("li");
+  IMBDId.textContent= IMBD;
+  movieInfo.appendChild(IMBDId);
+  const IMBDrating = document.createElement("li");
+  IMBDrating.textContent = rating;
+  movieInfo.appendChild(IMBDrating);
+  const cardAction = document.createElement("div");
+  cardAction.classList.add("card-action");
+  cardEl.appendChild(cardAction);
+  const externalLink = document.createElement("a");
+  externalLink.setAttribute("href",link);
+  
+}
+
+//boop
